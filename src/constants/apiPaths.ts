516622bc76d64ee1cd955aaf571b01cbc55d553f
Michael Shakmakov <mikeshakmakov@gmail.com>
2021-07-16 18:41:47 +0300

    task3 - products url and schema update

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://65wvl7vj6l.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://65wvl7vj6l.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://65wvl7vj6l.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://65wvl7vj6l.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://65wvl7vj6l.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,14 +1,29 @@
 import * as Yup from 'yup';
 
+// "URL": "https://shop.adidas.co.in/#!product/CM0081_pukaws",
+// "Product_Name": "Women's adidas Swim Puka Slippers",
+// "Product_ID": "CM0081",
+// "Listing_Price": 999,
+// "Sale_Price": 599,
+// "Discount": 40,
+// "Brand": "CORE / NEO",
+// "Description": "These adidas Puka slippers for women's come with slim straps for a great fit. Feature performance logo on the footbed and textured Rubber outsole that gives unique comfort.",
+// "Rating": 0,
+// "Reviews": 0,
+// "Images": [
+// "https://content.adidas.co.in/static/Product-CM0081/Men_SWIM_SLIDES_CM0081_1.jpg"
+
 export type Product = {
-  id: string,
-  title: string,
-  description: string,
-  price: number,
+  Product_ID: string,
+  Product_Name: string,
+  Description: string,
+  Listing_Price: number,
+  Images: string[];
 };
 
 export const ProductSchema = Yup.object().shape({
-  title: Yup.string().required(),
-  description: Yup.string(),
-  price: Yup.number().required(),
+  Product_ID: Yup.string().required(),
+  Product_Name: Yup.string().required(),
+  Description: Yup.string(),
+  Listing_Price: Yup.number().required(),
 });
